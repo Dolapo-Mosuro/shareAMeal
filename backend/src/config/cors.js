1776452cc2a6d0e726aaf9d240b3d1b3ef235d41
Dolapo Mosuro <dolapo@example.com>
2026-03-02 23:27:34 +0000

    Simplify CORS to only essential headers

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -2,9 +2,7 @@ const cors = require("cors");
 
 module.exports = () =>
 	cors({
-		origin: true, // Reflect the request origin
-		credentials: true,
-		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
-		optionsSuccessStatus: 200,
 	});
